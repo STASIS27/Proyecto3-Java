9688084c612f618ff869ae94b25f13fe52566bc1
Sergio Romero <stasisoficial@gmail.com>
2025-03-25 14:31:42 +0100

    Primer Commit

---
+++ src/Ventanas/VentanapPrincipal.java
@@ -0,0 +1,165 @@
+package Ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.Border;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class VentanapPrincipal extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textCorreo;
+	private JPanel panel;
+	private JButton btnRegistrarse;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanapPrincipal frame = new VentanapPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanapPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 64, 128));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		panel = new JPanel();
+		panel.setBounds(23, 26, 230, 211);
+		panel.setBorder(new LineBorder(Color.BLACK, 2, true));
+		contentPane.add(panel);
+		
+		
+		JLabel lblIniciarSesion = new JLabel("Iniciar Sesión");
+		lblIniciarSesion.setBounds(47, 2, 185, 99);
+		lblIniciarSesion.setForeground(new Color(0, 128, 255));
+		lblIniciarSesion.setFont(new Font("Segoe UI Black", Font.BOLD, 18));
+		
+		JLabel lblNewLabel = new JLabel("Correo");
+		lblNewLabel.setBounds(12, 88, 65, 25);
+		lblNewLabel.setForeground(new Color(0, 128, 255));
+		lblNewLabel.setFont(new Font("Segoe UI Black", Font.BOLD, 10));
+		
+		JLabel lblContrasea = new JLabel("Contraseña");
+		lblContrasea.setBounds(12, 127, 83, 25);
+		lblContrasea.setForeground(new Color(0, 128, 255));
+		lblContrasea.setFont(new Font("Segoe UI Black", Font.BOLD, 10));
+		
+		textCorreo = new JTextField();
+		textCorreo.setBounds(75, 92, 147, 19);
+		textCorreo.setFont(new Font("Segoe UI Black", Font.BOLD, 10));
+		textCorreo.setColumns(10);
+		
+		JButton btnIniciarSesion = new JButton("Iniciar Sesión");
+		btnIniciarSesion.setBounds(41, 162, 149, 29);
+		btnIniciarSesion.setForeground(new Color(0, 128, 255));
+		btnIniciarSesion.setFont(new Font("Segoe UI Black", Font.BOLD, 14));
+		btnIniciarSesion.setBackground(Color.WHITE);
+		panel.setLayout(null);
+		panel.add(lblNewLabel);
+		panel.add(lblIniciarSesion);
+		panel.add(textCorreo);
+		panel.add(lblContrasea);
+		panel.add(btnIniciarSesion);
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Segoe UI Black", Font.BOLD, 10));
+		passwordField.setBounds(75, 130, 147, 19);
+		panel.add(passwordField);
+		
+		JLabel lblNewLabel_1 = new JLabel("DAM FIT");
+		lblNewLabel_1.setForeground(new Color(0, 0, 128));
+		lblNewLabel_1.setFont(new Font("Segoe UI Black", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(75, 10, 126, 34);
+		panel.add(lblNewLabel_1);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(0, 128, 255));
+		panel_1.setBounds(10, 47, 416, 170);
+		panel_1.setBorder(new LineBorder(Color.BLACK, 2, true));
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblanNoTienes = new JLabel("¿Aún no \r\ntienes cuenta?");
+		lblanNoTienes.setBackground(new Color(240, 240, 240));
+		lblanNoTienes.setForeground(new Color(255, 255, 255));
+		lblanNoTienes.setFont(new Font("Segoe UI Black", Font.BOLD, 14));
+		lblanNoTienes.setBounds(244, -21, 182, 93);
+		panel_1.add(lblanNoTienes);
+		
+		btnRegistrarse = new JButton("Registrarse");
+		btnRegistrarse.addActionListener(this);
+		btnRegistrarse.setFont(new Font("Segoe UI Black", Font.BOLD, 14));
+		btnRegistrarse.setBackground(new Color(255, 255, 255));
+		btnRegistrarse.setForeground(new Color(0, 128, 255));
+		btnRegistrarse.setBounds(265, 82, 129, 29);
+		panel_1.add(btnRegistrarse);
+
+		
+		JLabel lblRegistrateMensaje = new JLabel("Registrate para poder");
+		lblRegistrateMensaje.setBounds(269, 39, 193, 13);
+		panel_1.add(lblRegistrateMensaje);
+		lblRegistrateMensaje.setForeground(new Color(255, 255, 255));
+		lblRegistrateMensaje.setFont(new Font("Segoe UI Black", Font.BOLD, 10));
+		
+		JLabel lbliniciarsesion = new JLabel("Iniciar Sesion");
+		lbliniciarsesion.setForeground(new Color(255, 255, 255));
+		lbliniciarsesion.setFont(new Font("Segoe UI Black", Font.BOLD, 10));
+		lbliniciarsesion.setBounds(289, 62, 147, 13);
+		panel_1.add(lbliniciarsesion);
+	}
+	public Border getPanelBorder() {
+		return panel.getBorder();
+	}
+	public void setPanelBorder(Border border) {
+		panel.setBorder(border);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		  if(e.getSource().equals(btnRegistrarse)) {
+		        VentanaRegistro vr = new VentanaRegistro();
+		        vr.setVisible(true);
+		        dispose(); 
+		
+	}
+}
+}
